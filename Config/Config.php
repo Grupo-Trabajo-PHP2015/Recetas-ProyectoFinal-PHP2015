3ca88f3409338610db291a67ddaa8304e7ce737b
Esteban Varela <bevarela6@misena.edu.co>
2015-03-27 19:53:57 -0500

    cambio en config

--- Config/Config.php
+++ Config/Config.php
@@ -3,7 +3,7 @@
     define("DRIVER","mysql");
     define("PORT", "3306");
     define("SERVER","localhost");
-    define("DBNAME","proyecto_restaurante");
+    define("DBNAME","db_restaurante");
     define("USER", "root");
     define("PASSWORD","");
    
