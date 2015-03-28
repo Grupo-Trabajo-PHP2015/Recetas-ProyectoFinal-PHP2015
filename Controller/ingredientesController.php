f28f2cccdbb91812eaa8e050e189146217fc111e
Esteban Varela <bevarela6@misena.edu.co>
2015-03-28 12:09:40 -0500

    Modal
    para registrar nuevos contactos

--- Controller/ingredientesController.php
+++ Controller/ingredientesController.php
@@ -4,10 +4,11 @@ require_once '../Config/Config.php';
 require_once '../Library/DataBase.php';
 require_once '../Model/ModelIngredientes.php';
 
-$id_n="";
-$nombre="";
-$url="";
-$descripcion="";
+$id_n = "";
+$nombre = "";
+$url = "";
+$descripcion2 = "";
+$bloqueo = "";
 
 if (isset($_GET["id"])) {
 
@@ -18,28 +19,28 @@ if (isset($_GET["id"])) {
     foreach ($model->Buscar() as $value) {
         $id_n = $value["idIngrediente"];
         $nombre = $value["Nombre"];
-        $descripcion = $value["Desripcion"];
+        $descripcion2 = $value["Desripcion"];
         $url = $value['Url'];
+        $bloqueo = "disabled='true'";
     }
-}elseif (isset ($_GET['delete']) ) {
-    
+} elseif (isset($_GET['delete'])) {
+
     $model_e = new Ingredientes();
-     $ids = $_GET["delete"];
+    $ids = $_GET["delete"];
     $model_e->__SET("idIngrediente", $ids);
-    if ( $model_e->Eliminar() ) {
-        
-        $mensaje =  "Se elimino";
-           print "<script>alert('$mensaje')</script>";
-        
-    }  else {
-        
-        $mensaje =  "Error en la conexión";
-           print "<script>alert('$mensaje')</script>";
+    if ($model_e->Eliminar()) {
+
+        $mensaje = "Se elimino";
+        print "<script>alert('$mensaje')</script>";
+    } else {
+
+        $mensaje = "Error en la conexión";
+        print "<script>alert('$mensaje')</script>";
     }
 }
 
 if (isset($_POST['guardar'])) {
-   
+
     $archivo = $_FILES["archivo"]["tmp_name"];
 
     $destino = "../Archivos/" . $_FILES["archivo"]["name"];
@@ -47,48 +48,48 @@ if (isset($_POST['guardar'])) {
 
     if ($_FILES["archivo"]["type"] == "image/png" || $_FILES["archivo"]["type"] == "image/jpeg" || $_FILES["archivo"]["type"] == "image/jpg") {
 
-            $id= rand(1,200);
-            $model_g = new Ingredientes();
-            
-            $model_g->__SET('idIngrediente',$id);
-            $model_g->__SET('Nombre',$_POST['nombre']);
-            $model_g->__SET('Desripcion',$_POST['descripcion']);
-            $model_g->__SET('Url',$destino);
-            $model_g->__SET('Tipo',$_POST['clasificacion']);
-            if ( $model_g->Guardar() ) {
+        $id = rand(1, 200);
+        $model_g = new Ingredientes();
 
-                move_uploaded_file($archivo, $destino);     
-                         $mensaje = "Se guardo Satisfactoriamente";
-                           print "<script>alert('$mensaje')</script>";
-            }  else {
-                
-           $mensaje =  "Error en la conexión";
-           print "<script>alert('$mensaje')</script>";
-                
-            }
-            
+        $model_g->__SET('idIngrediente', $id);
+        $model_g->__SET('Nombre', $_POST['nombre']);
+        $model_g->__SET('Desripcion', $_POST['descripcion']);
+        $model_g->__SET('Url', $destino);
+        $model_g->__SET('Tipo', $_POST['clasificacion']);
+        if ($model_g->Guardar()) {
+
+            move_uploaded_file($archivo, $destino);
+            $mensaje = "Se guardo Satisfactoriamente";
+            print "<script>alert('$mensaje')</script>";
+        } else {
+
+            $mensaje = "Error en la conexión";
+            print "<script>alert('$mensaje')</script>";
+        }
     } else {
 
         echo "Error al guardar \n Solo se pueden imagenes con extención JPEG , PNG , JPG";
-        
     }
-}elseif (isset ( $_POST['editar'] ) ) {
-    
+} elseif (isset($_POST['editar'])) {
+
     $model_m = new Ingredientes();
-    $model_m->__SET('idIngrediente',$_GET['id']);
-    $model_m->__SET('Nombre',$_POST['nombre']);
-    $model_m->__SET('Desripcion',$_POST['url']);
-    
-    if ( $model_m->Modificar()  ) {
-        
-        $mensaje =  "Se modifico";
-           print "<script>alert('$mensaje')</script>";
-    }  else {
-        
-        $mensaje =  "Error en la conexión";
-           print "<script>alert('$mensaje')</script>";
+    $model_m->__SET('idIngrediente', $_GET['id']);
+    $model_m->__SET('Nombre', $_POST['nombre']);
+    $model_m->__SET('Desripcion', $_POST['descripcion2']);
+
+    if ($model_m->Modificar()) {
+        $id_n = "";
+        $nombre = "";
+        $url = "";
+        $descripcion2 = "";
+        $bloqueo = "";
+        $mensaje = "Se modifico";
+        print "<script>alert('$mensaje')</script>";
+    } else {
+
+        $mensaje = "Error en la conexión";
+        print "<script>alert('$mensaje')</script>";
     }
-    
 }
 
 $model = new Ingredientes();
@@ -98,10 +99,11 @@ foreach ($model->Listar() as $value) {
     $tabla .="<tr>";
 
     $tabla .="<td>" . $value['Nombre'] . "</td>";
-    $tabla .="<td>" . $value['Desripcion']. "</td>";
+    $tabla .="<td>" . $value['Desripcion'] . "</td>";
     $tabla .="<td>" . $value['Url'] . "</td>";
-    $tabla .="<td> <a href='ingredientesController.php?id=".$value['idIngrediente']."'  class='btn btn-primary btn-xs' role='button'> <span class='glyphicon glyphicon-pencil'></span> </a> </td>";
-    $tabla .="<td> <a href='ingredientesController.php?delete=".$value['idIngrediente']."' class='btn btn-danger btn-xs' role='button'>  <span class='glyphicon glyphicon-trash'></span> </a> </td>";
+    $tabla .="<td> <a href='ingredientesController.php?id=" . $value['idIngrediente'] . "'  class='btn btn-primary btn-xs' role='button'> <span class='glyphicon glyphicon-pencil'></span> </a> </td>";
+    $tabla .="<td> <a href='ingredientesController.php?delete=" . $value['idIngrediente'] . "' class='btn btn-danger btn-xs' role='button'>  <span class='glyphicon glyphicon-trash'></span> </a> </td>";
+    //$tabla .="<td> <img src='".$value['url de la imagen']."'   width='150' heigth='150'> </td>";
     $tabla .="</tr>";
 }
 
--- View/Ingredientes/ingredientes.php
+++ View/Ingredientes/ingredientes.php
@@ -22,67 +22,64 @@
                         <fieldset>
 
                             <!-- Form Name -->
-                            <legend style="text-align: center" >Nuevos ingredientes</legend>
+                            <legend style="text-align: center" >Editar ingredientes</legend>
+                            
+                            <div class="form-group">
+                                <label class="col-md-4 control-label" for="textinput">ID:</label>  
+                                <div class="col-md-6">
+                                    <input id="textinput" name="id"   type="text"  placeholder="Nombre ingrediente" class="form-control input-md" <?php echo $bloqueo ?> value=" <?php echo $id_n; ?> " >
 
+                                </div>
+                            </div>
+                            
                             <!-- Text input-->
                             <div class="form-group">
                                 <label class="col-md-4 control-label" for="textinput">Nombre:</label>  
                                 <div class="col-md-6">
-                                    <input id="textinput" name="nombre" type="text" placeholder="Nombre ingrediente" class="form-control input-md" autofocus="" value=" <?php echo $nombre; ?> " >
+                                    <input id="textinput" name="nombre" type="text" placeholder="Nombre ingrediente" class="form-control input-md" value=" <?php echo $nombre; ?> " >
 
-                                </div>
-                            </div>
-
-                            <!-- Textarea -->
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="textarea">Descripción:</label>
-                                <div class="col-md-4">                     
-                                    <textarea class="form-control" id="textarea" name="descripcion"  placeholder="Caracteristicas del ingrediente o producto" rows="4" cols="150" > <?php echo $descripcion; ?> </textarea>
                                 </div>
                             </div>
 
                             <!-- Text input-->
                             <div class="form-group">
+                                <label class="col-md-4 control-label" for="textinput">Descripción:</label>  
+                                <div class="col-md-6">
+                                    <input id="textinput" name="descripcion2" type="text" placeholder="Caracteristicas del producto" class="form-control input-md"  value=" <?php echo $descripcion2; ?> " >
+
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
                                 <label class="col-md-4 control-label" for="textinput">URL:</label>  
                                 <div class="col-md-6">
-                                    <input id="textinput" name="url" type="text" placeholder="//imagen.cc" class="form-control input-md" autofocus="" value=" <?php echo $url; ?> " >
+                                    <input id="textinput" name="url"  <?php echo $bloqueo ?> type="text" placeholder="Dirección imagen" class="form-control input-md"  value=" <?php echo $url; ?> " >
 
                                 </div>
                             </div>
 
-                            <!-- File Button --> 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="filebutton">Insertar imagen:</label>
-                                <div class="col-md-4">
-                                    <input  class="input-file" type="file" name="archivo" >
-                                </div>
-                            </div>
-                            <!-- Select Basic -->
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="clasificacion">Clasificación</label>
-                                <div class="col-md-5">
-                                    <select id="clasificacion" name="clasificacion" class="form-control">
-                                        <option value="1">Salsas</option>
-                                        <option value="2">Carnes</option>
-                                        <option value="3">Verduras</option>
-                                        <option value="4">Granos</option>
-                                    </select>
-                                </div>
-                            </div>
+                           
                             <!-- Button -->
-                            <div class="btn-group ">
-                               
+                            <div class="form-group">
+                                <label class="col-md-4 control-label" for="editar"></label>
                                 <div class="col-md-4">
                                     <button id="editar" name="editar" class="btn btn-success">Editar</button>
-                                </div>
-                                
-                                <div class="col-md-4">
-                                    <button id="singlebutton" type="submit" name="guardar" class="btn btn-primary">Guardar</button>
                                 </div>
                             </div>
 
                         </fieldset>
                     </form>
+
+                </div>
+                <div class="col-md-4">
+                    
+                    <center>
+        <div class="container">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#nuevo"  style="margin-right: 40%;margin-top: 10%;" >
+                Agregar nuevos ingredientes
+            </button>
+    </center>
 
                 </div>	
             </div>
@@ -111,10 +108,82 @@
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="row">
 
-        <script type="text/javascript" src="../Assets/js/jquery-1.11.2.js" ></script>
-        <script type="text/javascript" src="../Assets/bootstrap-3.3.4-dist/js/bootstrap.js" ></script>
+                <!-- Modal -->
+                <div class="modal fade" id="nuevo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel"  style="text-align: center" >Nuevos ingredientes</h4>
+                            </div>
+                            <div class="modal-body">
+                                    <form class="form-horizontal"  method="POST" action="" enctype="multipart/form-data" >
+                                        <fieldset>
+
+                                            <!-- Text input-->
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label" for="textinput">Nombre:</label>  
+                                                <div class="col-md-6">
+                                                    <input id="textinput" name="nombre" type="text" placeholder="Nombre ingrediente" class="form-control input-md" autofocus="" value=" " >
+
+                                                </div>
+                                            </div>
+
+                                            <!-- Textarea -->
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label" for="textarea">Descripción:</label>
+                                                <div class="col-md-4">                     
+                                                    <textarea class="form-control" id="textarea" name="descripcion"  placeholder="Caracteristicas del ingrediente o producto" rows="4" cols="150" > </textarea>
+                                                </div>
+                                            </div>
+
+                                            <!-- File Button --> 
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label" for="filebutton">Insertar imagen:</label>
+                                                <div class="col-md-4">
+                                                    <input  class="input-file" type="file" name="archivo" >
+                                                </div>
+                                            </div>
+                                            <!-- Select Basic -->
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label" for="clasificacion">Clasificación</label>
+                                                <div class="col-md-5">
+                                                    <select id="clasificacion" name="clasificacion" class="form-control">
+                                                        <option value="1">Salsas</option>
+                                                        <option value="2">Carnes</option>
+                                                        <option value="3">Verduras</option>
+                                                        <option value="4">Granos</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                     
+                                            <!-- Button -->
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label" for="guardar"></label>
+                                                <div class="col-md-4">
+                                                    <button id="guardar" name="guardar" class="btn btn-primary">Guardar</button>
+                                                </div>
+                                            </div>
+
+                                        </fieldset>
+                                    </form>
+                               
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+
+            <script type="text/javascript" src="../Assets/js/jquery-1.11.2.js" ></script>
+            <script type="text/javascript" src="../Assets/bootstrap-3.3.4-dist/js/bootstrap.js" ></script>
 
     </body>
 </html>
